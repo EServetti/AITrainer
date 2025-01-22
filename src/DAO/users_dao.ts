ca64e74a22eb5c938d8d984cac40313d669d2c37
unknown <eservetti2018@gmail.com>
2025-01-22 14:29:24 -0300

    Agregado limite de conexiones

--- src/DAO/users_dao.ts
+++ src/DAO/users_dao.ts
@@ -9,6 +9,7 @@ const database = mysql2.createPool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
 });
 
 export async function createUser(data: User): Promise<any> {
